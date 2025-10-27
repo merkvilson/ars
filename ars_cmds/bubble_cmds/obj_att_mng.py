56e3fa86ab70161e2d6fc5a6c9d4af62332e2efe
merkvilson <g.merkvilson@gmail.com>
2025-10-27 20:17:44 +0400

    added drag icon

--- ars_cmds/bubble_cmds/obj_att_mng.py
+++ ars_cmds/bubble_cmds/obj_att_mng.py
@@ -2,14 +2,14 @@ from ui.widgets.context_menu import ContextMenuConfig, open_context
 from theme.fonts import font_icons as ic
 from ars_cmds.core_cmds.run_ext import run_ext
 import os
-from PyQt6.QtGui import QCursor
+from PyQt6.QtGui import QCursor, QColor
 import random
 from ars_cmds.obj_ctx.obj_color_ctx import obj_color
 from ars_cmds.obj_ctx.obj_psr import obj_scale
 from ars_cmds.obj_ctx.obj_prompt import prompt_ctx
 from ars_cmds.bubble_cmds.delete_selected_obj import BBL_TRASH  as delete_selected_obj
 from ars_cmds.bubble_cmds.open_prompt import BBL_PROMPT as open_prompt
-
+from PyQt6.QtCore import QPoint
 
 
 def obj_att_mng(self, ):
@@ -35,8 +35,16 @@ def obj_att_mng(self, ):
                         ic.ICON_GIZMO_SCALE_3D: lambda: obj_scale(self,mouse_pos, obj_att_mng),
                         ic.ICON_TRASH: lambda: delete_selected_obj(self, None),
                         ic.ICON_TEXT_INPUT: lambda: prompt_ctx(self, mouse_pos, selected[0], obj_att_mng),
-                        }
-    
+        ic.ICON_CLOSE_RADIAL: lambda: (ctx.close_animated(),self.viewport.controller.set_handles(['t'])),
+    }
+
+    config.callbackR = { ic.ICON_CLOSE_RADIAL: lambda value: ctx.move(  self.central_widget.mapFromGlobal(QCursor.pos())- QPoint(ctx.width()//2, ctx.height() - config.item_radius) )
+    }
+    config.slider_values = {ic.ICON_CLOSE_RADIAL: (0,1,0),}
+    config.slider_color = {ic.ICON_CLOSE_RADIAL: QColor(150, 150, 150, 0)}
+
+
+
 
     ctx = open_context(
         parent=self.central_widget,
