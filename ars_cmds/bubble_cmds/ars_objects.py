ebe8733ba4a2fe31fc57ed6dc4a8f36b86e55145
merkvilson <g.merkvilson@gmail.com>
2025-10-28 08:36:00 +0400

    added new sprite icon

--- ars_cmds/bubble_cmds/ars_objects.py
+++ ars_cmds/bubble_cmds/ars_objects.py
@@ -9,7 +9,7 @@ def BBL_OBJECT(self, position):
     config = ContextMenuConfig()
 
     options_list =  [
-                    ic.ICON_SPRITE,
+                     ic.ICON_OBJ_SPRITE,
                      ic.ICON_OBJ_BOX,
                      ic.ICON_OBJ_SPHERE,
                      ic.ICON_OBJ_CYLINDER,
@@ -22,7 +22,7 @@ def BBL_OBJECT(self, position):
                      ]
 
     config.callbackL = {
-        ic.ICON_SPRITE:            lambda: add_sprite(self, animated=True),
+        ic.ICON_OBJ_SPRITE:        lambda: add_sprite(self, animated=True),
         ic.ICON_OBJ_BOX:           lambda: add_mesh(self, os.path.join("res","mesh files", 'box.obj'      ), animated = True),
         ic.ICON_OBJ_SPHERE:        lambda: add_mesh(self, os.path.join("res","mesh files", 'sphere.obj'   ), animated = True),
         ic.ICON_OBJ_CYLINDER:      lambda: add_mesh(self, os.path.join("res","mesh files", 'cylinder.obj' ), animated = True),
@@ -35,7 +35,7 @@ def BBL_OBJECT(self, position):
     }
 
     config.additional_texts = {
-        ic.ICON_SPRITE: '2D Sprite',
+        ic.ICON_OBJ_SPRITE: '2D Sprite',
         ic.ICON_OBJ_BOX:   'Cube', 
         ic.ICON_OBJ_SPHERE: 'Sphere', 
         ic.ICON_OBJ_CYLINDER: 'Cylinder',
@@ -48,7 +48,7 @@ def BBL_OBJECT(self, position):
     }
 
     config.hotkey_items = {
-        ic.ICON_SPRITE: 'D',
+        ic.ICON_OBJ_SPRITE: 'D',
         ic.ICON_OBJ_BOX:    'C', 
         ic.ICON_OBJ_SPHERE:  'S', 
         ic.ICON_OBJ_CYLINDER: 'L',
