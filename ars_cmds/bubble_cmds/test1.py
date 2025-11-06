64adafced89141d834f1367ade1f8acfc1665173
merkvilson <g.merkvilson@gmail.com>
2025-11-06 07:29:22 +0400

    refactor main function to improve context menu configuration and image handling

--- ars_cmds/bubble_cmds/test1.py
+++ ars_cmds/bubble_cmds/test1.py
@@ -4,6 +4,8 @@ from ars_cmds.core_cmds.run_ext import run_ext
 from PyQt6.QtGui import QCursor
 from ars_cmds.core_cmds.load_object import selected_object
 from PyQt6.QtCore import QPoint
+from prefs.pref_controller import get_path
+import os
 
 BBL_TEST_CONFIG = {"symbol": ic.ICON_TEST}
 def BBL_TEST(self, position):
@@ -11,11 +13,35 @@ def BBL_TEST(self, position):
 
 def main(self):
 
-    if not selected_object(self):
-        return
-    else: obj = selected_object(self)
+    config = ContextMenuConfig()
+
+    config.slider_values = {
+        'A': (0, 100, 50),
+    }
+
+    def set_img_by_index(val):
+        val = int(val)
+        print(f'Slider A changed to {int(val)}')
+        images_path = get_path("steps")
+        images_list = os.listdir(images_path)
+        if images_list:
+            index = val * (len(images_list) - 1) // 100
+            image_path = os.path.join(images_path, images_list[index])
+
+
+        self.viewport.bg.set_image(image_path)
     
-    obj.set_shading(None)
+    config.callbackL = {
+        'A': lambda val: set_img_by_index(val),
+        }
+
+
+    ctx = open_context(
+        parent=self.central_widget,
+        items=['A'],
+        position=None,
+        config=config
+    )
 
 def execute_plugin(window):
     main(window)
