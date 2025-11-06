59927df319507fc47632986bfe8e06568b79fec0
merkvilson <g.merkvilson@gmail.com>
2025-11-06 17:15:52 +0400

    TODO: Replace temporal solution with proper logic.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "1"
-os.environ["QT_FONT_DPI"] = "200"
+os.environ["QT_FONT_DPI"] = "145"
 os.environ["QT_QPA_PLATFORM"] = "windows:fontengine=freetype"
 os.environ['QT_LOGGING_RULES'] = 'qt.multimedia*=false'
 
