f40fe3655442b407ea25ef076df4d679a6c6c6b2
merkvilson <g.merkvilson@gmail.com>
2025-11-07 09:23:50 +0400

    new button

--- ars_cmds/bubble_cmds/test quick.py
+++ ars_cmds/bubble_cmds/test quick.py
@@ -11,32 +11,14 @@ def BBL_TEST2(self, position):
 
     
 def main(self, position):
-    
-    if not selected_object(self):
-        return
-    else: default_object = selected_object(self)
-
-
-
     config = ContextMenuConfig()
-    config.auto_close = False
-    config.custom_height = 150
-    config.custom_width = 410
 
-    options_list = [ "text_widget" ]
-
-    def set_text_from_prompt():
-        default_object.set_text(text_widget.text_edit.toPlainText())
-
-    text_widget = MultiLineInputWidget()
-    text_widget.text_edit.setPlainText(default_object.prompt)
-    text_widget.text_edit.textChanged.connect(set_text_from_prompt)
-
-    text_widget.setFixedSize(400, 140)
-
-    config.custom_widget_items = {"text_widget": text_widget}
+    options_list = [ "a", "b", "c" ]
 
 
+    config.callbackL = {"a": lambda: print("Option A selected"),
+                        "b": lambda: print("Option B selected"),
+                        "c": lambda: print("Option C selected")}
 
     ctx = open_context(
         parent=self.central_widget,
