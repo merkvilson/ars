19b4b1a2c2dc8e6d95b55b86124d12df6c71e095
merkvilson <g.merkvilson@gmail.com>
2025-11-10 09:57:55 +0400

    refactor key_check function to add mouse button checks

--- ars_cmds/core_cmds/key_check.py
+++ ars_cmds/core_cmds/key_check.py
@@ -5,11 +5,18 @@ from PyQt6.QtCore import Qt
 def key_check(key):
     key = key.lower()
 
+    # Check keyboard modifiers
     modifiers = QApplication.keyboardModifiers()
     if   key == "shift": result = modifiers & Qt.KeyboardModifier.ShiftModifier
     elif key == "ctrl":  result = modifiers & Qt.KeyboardModifier.ControlModifier
     elif key == "alt":   result = modifiers & Qt.KeyboardModifier.AltModifier
     elif key == "meta":  result = modifiers & Qt.KeyboardModifier.MetaModifier
+    
+    # Check mouse buttons
+    elif key == "l":   result = QApplication.mouseButtons() & Qt.MouseButton.LeftButton
+    elif key == "r":  result = QApplication.mouseButtons() & Qt.MouseButton.RightButton
+    elif key == "m": result = QApplication.mouseButtons() & Qt.MouseButton.MiddleButton
+    
     else: result = None
 
     return result
