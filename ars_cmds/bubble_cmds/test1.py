8fe9243fd79840307fd7605b336f0817317b34e2
merkvilson <g.merkvilson@gmail.com>
2025-11-10 09:24:58 +0400

    updated test bbl

--- ars_cmds/bubble_cmds/test1.py
+++ ars_cmds/bubble_cmds/test1.py
@@ -1,21 +1,33 @@
 from ui.widgets.context_menu import ContextMenuConfig, open_context
 from theme.fonts import font_icons as ic
+from ars_cmds.core_cmds.run_ext import run_ext
+from PyQt6.QtGui import QCursor
+from ars_cmds.core_cmds.load_object import selected_object
+from PyQt6.QtCore import QPoint, QTimer
 
 
 BBL_TEST_CONFIG ={"symbol": ic.ICON_TEST }
 def BBL_TEST(self, position):
+    run_ext(__file__, self)
+
+
+def main(self, position):
     config = ContextMenuConfig()
 
 
     options_list = [
-        ["1", "2", "3"],
+        ["1", "2"],
     ]
  
 
     config.additional_texts = {
-    "1": "Option 1",
-    "2": "Option 2",
-    "3": "Option 3",
+    "1": "Button 1",
+    "2": "Button 2",
+    }
+
+    config.callbackL = {
+        "1": lambda: print("Button 1 pressed"),
+        "2": lambda: print("Button 2 pressed"),
     }
 
     ctx = open_context(
@@ -24,3 +36,7 @@ def BBL_TEST(self, position):
         position=position,
         config=config
     )
+
+
+def execute_plugin(window):
+    main(window, position=window.central_widget.mapFromGlobal(QCursor.pos()))
