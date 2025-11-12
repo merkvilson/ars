6308adce9a263858a825ab3247f69fb97902f4b8
merkvilson <g.merkvilson@gmail.com>
2025-11-12 09:45:23 +0400

    new button for testing new incremental value feature

---
+++ ars_cmds/bubble_cmds/test2.py
@@ -0,0 +1,51 @@
+from ars_cmds.core_cmds.key_check import key_check_continuous
+from ui.widgets.context_menu import ContextMenuConfig, open_context
+from theme.fonts import font_icons as ic
+from ars_cmds.core_cmds.run_ext import run_ext
+from PyQt6.QtGui import QCursor
+from ars_cmds.core_cmds.load_object import selected_object
+from PyQt6.QtCore import QPoint, QTimer
+
+
+def BBL_2(self, position):
+    run_ext(__file__, self)
+
+
+def main(self, position):
+    config = ContextMenuConfig()
+    config.show_value = True
+
+    config.slider_values = {
+        "1": (0, 100, 50),
+    }
+    config.auto_close = False
+    config.incremental_values = {
+        "1": 1,
+    }
+
+    options_list = [
+        ["1", "2"],
+    ]
+ 
+
+    config.additional_texts = {
+    "1": "Slider",
+    "2": "Button 2",
+    }
+
+
+    config.callbackL = {
+        "1": lambda value: print(value),
+        "2": lambda: print("Button 2 pressed"),
+    }
+
+    ctx = open_context(
+        parent=self.central_widget,
+        items=options_list,
+        position=position,
+        config=config
+    )
+
+
+def execute_plugin(window):
+    main(window, position=window.central_widget.mapFromGlobal(QCursor.pos()))
