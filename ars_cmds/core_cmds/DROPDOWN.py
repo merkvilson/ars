0f5f074271e62fa482acbe5923e8fa5c3e68c7d7
merkvilson <g.merkvilson@gmail.com>
2025-11-16 03:21:29 +0400

    Enhance r_dropdown functionality to open files in the code editor and close existing context menus

--- ars_cmds/core_cmds/DROPDOWN.py
+++ ars_cmds/core_cmds/DROPDOWN.py
@@ -1,10 +1,10 @@
-from ui.widgets.context_menu import ContextMenuConfig, open_context
+from ui.widgets.context_menu import ContextMenuConfig, open_context, find_all_open_context_menus
 from PyQt6.QtGui import QCursor
 from PyQt6.QtCore import Qt, QTimer
 from theme.fonts import font_icons as ic
 import theme.fonts.new_fonts as RRRFONT
 from ars_cmds.util_cmds.open_file import open_file
-
+from ars_cmds.bubble_cmds import py_scripts
 def unlock_function(self):
     new_state = Qt.CheckState.Unchecked if self.bubbles_overlay.locked else Qt.CheckState.Checked
     self.bubbles_overlay.toggle_lock(new_state.value)
@@ -36,12 +36,21 @@ def r_dropdown(self, code_path = None):
             ic.ICON_LOCK_state: not self.bubbles_overlay.locked,
     }
 
+    def open_in_arseditor(path):
+        for ctx_menu in find_all_open_context_menus():
+            if ctx_menu.symbol == "py_scripts_ctx":
+                ctx_menu.close()
+        py_ctx_menu, code_editor = py_scripts.execute_plugin(self)
+        with open(path, 'r', encoding='utf-8') as f:
+            code_file = f.read()
+        code_editor.editor.setPlainText(code_file)
+
     config.callbackL = {
         ic.ICON_LOCK_state:  lambda: (
                                unlock_function(self),
                                ctx.update_item(ic.ICON_LOCK_state, "symbol", ic.ICON_LOCK if self.bubbles_overlay.locked else ic.ICON_LOCK_OPEN),
             ),
-        ic.ICON_CODE_TERMINAL: lambda: open_file(code_path),
+        ic.ICON_CODE_TERMINAL: lambda: open_in_arseditor(code_path),
         ic.ICON_POWER: lambda: QTimer.singleShot(500, self.close),
     }
 
