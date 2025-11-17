0a4716132c366249648f8930b8fffb4efa6ec320
merkvilson <g.merkvilson@gmail.com>
2025-11-17 23:07:44 +0400

    Refactor time_cmd.py to improve delay function and enhance usage examples

--- ars_cmds/util_cmds/time_cmd.py
+++ ars_cmds/util_cmds/time_cmd.py
@@ -2,3 +2,30 @@ from PyQt6.QtCore import QTimer
 
 def after(ms, cmd):
     QTimer.singleShot(ms, cmd)
+
+
+def delay(cmd):
+    def wrapper(duration = 1000, *args, **kwargs):
+        value = QTimer.singleShot(duration, lambda: cmd(*args, **kwargs))
+        return value
+    return wrapper
+
+"""
+
+import random
+@delay
+def doit():
+    msg(random.randint(0,100))
+
+# Example usage: 
+# doit(duration = 5000)
+# Alternative
+
+def doit():
+    msg(random.randint(0,100))
+
+delay(doit)(5000) usage with after:
+
+# alternative usage: after(5000, doit)
+
+"""
