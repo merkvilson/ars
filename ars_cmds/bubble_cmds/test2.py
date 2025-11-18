82a8e21f2f7c15555cfbb5f355eae8670a62934b
merkvilson <g.merkvilson@gmail.com>
2025-11-18 23:28:16 +0400

    Refactor execute_plugin to remove unused color picker functionality and clean up code

--- ars_cmds/bubble_cmds/test2.py
+++ ars_cmds/bubble_cmds/test2.py
@@ -2,10 +2,6 @@ from ui.widgets.context_menu import ContextMenuConfig, open_context
 from theme.fonts import font_icons as ic
 from ars_cmds.core_cmds.run_ext import run_ext
 
-from ui.widgets.screen_color_picker import ScreenshotOverlay
-from PyQt6.QtWidgets import QApplication
-
-
 def BBL_TEST2(*args):
     run_ext(__file__)
 
@@ -17,9 +13,8 @@ def execute_plugin(ars_window):
     config.close_on_outside = False
 
     config.options = {
-    "1": "Slider A",
     "2": "Slider B",
-    ic.ICON_COLOR_PICKER: "Color Picker"
+    "1": "Slider A",
     }
 
     config.slider_values = {
@@ -35,18 +30,7 @@ def execute_plugin(ars_window):
     config.callbackL = {
         "1": lambda value: print(value),
         "2": lambda value: print(value),
-        ic.ICON_COLOR_PICKER: lambda: start_picking(ctx),
     }
 
-
-    def display_color_callback(color):
-        ctx.update_item(ic.ICON_COLOR_PICKER, "color", color)
-
-    def start_picking(ctx):
-        
-        # Capture entire screen
-        screen = QApplication.primaryScreen()
-        screenshot = screen.grabWindow(0)
-        ctx.overlay = ScreenshotOverlay(screenshot, paretn_callback=display_color_callback)
 
     ctx = open_context(config)
