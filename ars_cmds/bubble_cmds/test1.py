3e6232500dd1e3133ceb001be5fb750dc96bb290
merkvilson <g.merkvilson@gmail.com>
2025-11-18 18:22:03 +0400

    Refactor color picker integration in context menu for improved functionality

--- ars_cmds/bubble_cmds/test1.py
+++ ars_cmds/bubble_cmds/test1.py
@@ -1,10 +1,6 @@
 from ui.widgets.context_menu import ContextMenuConfig, open_context
 from ars_cmds.core_cmds.run_ext import run_ext
 from theme.fonts import font_icons as ic
-from ui.widgets.screen_color_picker import ScreenshotOverlay
-from PyQt6.QtWidgets import QApplication
-
-def BBL_TEST(*args): run_ext(__file__)
 
 def execute_plugin(ars_window):
     config = ContextMenuConfig()
@@ -13,19 +9,8 @@ def execute_plugin(ars_window):
     config.options = {
         ic.ICON_TEST: "Option A",
         ic.ICON_TEST2: "Option B",
-        ic.ICON_TEST3: "Color Picker",}
+        ic.ICON_TEST3: "Option C",}
     
 
-    def display_color_callback(color):
-        ctx.update_item(ic.ICON_TEST3, "color", color)
-
-    def start_picking(ctx):
-        
-        # Capture entire screen
-        screen = QApplication.primaryScreen()
-        screenshot = screen.grabWindow(0)
-        ctx.overlay = ScreenshotOverlay(screenshot, paretn_callback=display_color_callback)
-
-    config.callbackL = { ic.ICON_TEST3: lambda: start_picking(ctx),}
 
     ctx = open_context(config)
--- ars_cmds/bubble_cmds/test2.py
+++ ars_cmds/bubble_cmds/test2.py
@@ -2,6 +2,8 @@ from ui.widgets.context_menu import ContextMenuConfig, open_context
 from theme.fonts import font_icons as ic
 from ars_cmds.core_cmds.run_ext import run_ext
 
+from ui.widgets.screen_color_picker import ScreenshotOverlay
+from PyQt6.QtWidgets import QApplication
 
 
 def BBL_TEST2(*args):
@@ -15,6 +17,7 @@ def execute_plugin(ars_window):
     config.options = {
     "1": "Slider A",
     "2": "Slider B",
+    ic.ICON_COLOR_PICKER: "Color Picker"
     }
 
     config.slider_values = {
@@ -30,6 +33,18 @@ def execute_plugin(ars_window):
     config.callbackL = {
         "1": lambda value: print(value),
         "2": lambda value: print(value),
+        ic.ICON_COLOR_PICKER: lambda: start_picking(ctx),
     }
+
+
+    def display_color_callback(color):
+        ctx.update_item(ic.ICON_COLOR_PICKER, "color", color)
+
+    def start_picking(ctx):
+        
+        # Capture entire screen
+        screen = QApplication.primaryScreen()
+        screenshot = screen.grabWindow(0)
+        ctx.overlay = ScreenshotOverlay(screenshot, paretn_callback=display_color_callback)
 
     ctx = open_context(config)
