45587115c6d3371c811f0ee9204b599a8115a4b6
merkvilson <g.merkvilson@gmail.com>
2025-11-20 18:00:52 +0400

    Remove unused context menu configuration and retain Hello World message

--- ars_scripts/user/test_script_1.py
+++ ars_scripts/user/test_script_1.py
@@ -1,13 +1,1 @@
-from ui.widgets.context_menu import open_context, ContextMenuConfig
-
-config = ContextMenuConfig()
-config.custom_width = ars_window.width() 
-config.options = {"1": "1", "2": "2", "3": "3", "4": "4"}
-
-config.slider_values = {"1": (50,500,50), "2": (50,500,50), "3": (50,500,50), "4": (50,500,50)}
-
-
-config.callbackL={"1": lambda value: ctx.resize_top(value)}
-config.incremental_value= (-10,"y")
-
-ctx = open_context(config)
+msg("Hello World")
