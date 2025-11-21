f3bd7b29e0dd14c471300ecf06bea3b061dec5af
merkvilson <g.merkvilson@gmail.com>
2025-11-21 23:31:59 +0400

    reformat this file to remove extra spaces, fix spacing around colons and operators, and align it with PEP 8.

--- ars_cmds/bubble_cmds/ars_objects.py
+++ ars_cmds/bubble_cmds/ars_objects.py
@@ -3,9 +3,17 @@ from ui.widgets.context_menu import ContextMenuConfig, open_context
 from theme.fonts import font_icons as ic
 from ars_cmds.core_cmds.run_ext import run_ext
 from PyQt6.QtGui import QCursor
-from ars_cmds.core_cmds.load_object import add_mesh, add_sprite, add_text3d, add_primitive
+from ars_cmds.core_cmds.load_object import (
+    add_mesh,
+    add_sprite,
+    add_text3d,
+    add_primitive,
+)
+
 
 BBL_OBJECT_CONFIG = {"symbol": ic.ICON_OBJ_BBOX, "hotkey": "G"}
+
+
 def BBL_OBJECT(*args):
     run_ext(__file__)
 
@@ -14,49 +22,48 @@ def execute_plugin(ars_window):
     config = ContextMenuConfig()
 
     config.options = {
-        ic.ICON_OBJ_TXT_ABC:  'Text',
-        ic.ICON_OBJ_SPRITE:   '2D Sprite',
-        ic.ICON_OBJ_BOX:      'Cube', 
-        ic.ICON_OBJ_SPHERE:   'Sphere', 
+        ic.ICON_OBJ_TXT_ABC: 'Text',
+        ic.ICON_OBJ_SPRITE: '2D Sprite',
+        ic.ICON_OBJ_BOX: 'Cube',
+        ic.ICON_OBJ_SPHERE: 'Sphere',
         ic.ICON_OBJ_CYLINDER: 'Cylinder',
-        ic.ICON_OBJ_CONE:     'Cone',
-        ic.ICON_OBJ_PYRAMID:  'Pyramid',
-        ic.ICON_OBJ_PLANE:    'Plane',
-        ic.ICON_OBJ_DISC:     'Disc',
-        ic.ICON_OBJ_TORUS:    'Torus',
-        ic.ICON_FILE_3D:      'Load Object',
-        ic.ICON_ORIGAMI:      'Test Mesh',
+        ic.ICON_OBJ_CONE: 'Cone',
+        ic.ICON_OBJ_PYRAMID: 'Pyramid',
+        ic.ICON_OBJ_PLANE: 'Plane',
+        ic.ICON_OBJ_DISC: 'Disc',
+        ic.ICON_OBJ_TORUS: 'Torus',
+        ic.ICON_FILE_3D: 'Load Object',
+        ic.ICON_ORIGAMI: 'Test Mesh',
     }
 
     config.callbackL = {
-        ic.ICON_OBJ_TXT_ABC:       lambda: add_text3d(),
-        ic.ICON_OBJ_SPRITE:        lambda: add_sprite(animated=True),
-        ic.ICON_OBJ_BOX:           lambda: add_primitive(primitive_type='cube', animated=True         ),
-        ic.ICON_OBJ_SPHERE:        lambda: add_primitive(primitive_type='sphere', animated=True       ),
-        ic.ICON_OBJ_CYLINDER:      lambda: add_primitive(primitive_type='cylinder', animated=True     ),
-        ic.ICON_OBJ_CONE:          lambda: add_primitive(primitive_type='cone', animated=True         ),
-        ic.ICON_OBJ_PYRAMID:       lambda: add_primitive(primitive_type='pyramid', animated=True      ),
-        ic.ICON_OBJ_PLANE:         lambda: add_primitive(primitive_type='plane', animated=True        ),
-        ic.ICON_OBJ_DISC:          lambda: add_primitive(primitive_type='disc', animated=True         ),
-        ic.ICON_OBJ_TORUS:         lambda: add_primitive(primitive_type='torus', radius_inner = 0.25, animated=True),
-        ic.ICON_ORIGAMI:           lambda: add_mesh(os.path.join("res","mesh files", "origami.obj"  ), animated = True),
-        ic.ICON_FILE_3D:           lambda: add_mesh(),
+        ic.ICON_OBJ_TXT_ABC: lambda: add_text3d(),
+        ic.ICON_OBJ_SPRITE: lambda: add_sprite(animated=True),
+        ic.ICON_OBJ_BOX: lambda: add_primitive(primitive_type="cube", animated=True),
+        ic.ICON_OBJ_SPHERE: lambda: add_primitive(primitive_type="sphere", animated=True),
+        ic.ICON_OBJ_CYLINDER: lambda: add_primitive(primitive_type="cylinder", animated=True),
+        ic.ICON_OBJ_CONE: lambda: add_primitive(primitive_type="cone", animated=True),
+        ic.ICON_OBJ_PYRAMID: lambda: add_primitive(primitive_type="pyramid", animated=True),
+        ic.ICON_OBJ_PLANE: lambda: add_primitive(primitive_type="plane", animated=True),
+        ic.ICON_OBJ_DISC: lambda: add_primitive(primitive_type="disc", animated=True),
+        ic.ICON_OBJ_TORUS: lambda: add_primitive(primitive_type="torus", radius_inner=0.25, animated=True),
+        ic.ICON_ORIGAMI: lambda: add_mesh(os.path.join("res", "mesh files", "origami.obj"),animated=True,),
+        ic.ICON_FILE_3D: lambda: add_mesh(),
     }
 
     config.hotkey_items = {
-        ic.ICON_OBJ_TXT_ABC:    'A',
-        ic.ICON_OBJ_SPRITE:     '2',
-        ic.ICON_OBJ_BOX:        'C', 
-        ic.ICON_OBJ_SPHERE:     'S', 
-        ic.ICON_OBJ_CYLINDER:   'L',
-        ic.ICON_OBJ_CONE:       'Q',
-        ic.ICON_OBJ_PYRAMID:    'Y',
-        ic.ICON_OBJ_PLANE:      'P',
-        ic.ICON_OBJ_DISC:       'D',
-        ic.ICON_OBJ_TORUS:      'T',
-        ic.ICON_FILE_3D:        'L',
-        ic.ICON_ORIGAMI:        'O',
-
+        ic.ICON_OBJ_TXT_ABC: "A",
+        ic.ICON_OBJ_SPRITE: "2",
+        ic.ICON_OBJ_BOX: "C",
+        ic.ICON_OBJ_SPHERE: "S",
+        ic.ICON_OBJ_CYLINDER: "L",
+        ic.ICON_OBJ_CONE: "Q",
+        ic.ICON_OBJ_PYRAMID: "Y",
+        ic.ICON_OBJ_PLANE: "P",
+        ic.ICON_OBJ_DISC: "D",
+        ic.ICON_OBJ_TORUS: "T",
+        ic.ICON_FILE_3D: "L",
+        ic.ICON_ORIGAMI: "O",
     }
 
     ctx = open_context(config)
