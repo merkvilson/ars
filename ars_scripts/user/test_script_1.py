f354d517a4ce11a08b73c4b0f42f55fd7e399fe7
merkvilson <g.merkvilson@gmail.com>
2025-11-22 18:54:51 +0400

    fix context menu options and callbacks for primitive shapes

--- ars_scripts/user/test_script_1.py
+++ ars_scripts/user/test_script_1.py
@@ -1,10 +1,14 @@
 from ui.widgets.context_menu import open_context, ContextMenuConfig
 
 config=ContextMenuConfig()
-config.auto_close=False
 config.options = {
-                ic.ICON_OBJ_PYRAMID:"Cube", 
+                ic.ICON_OBJ_BOX:"Cube", 
                 ic.ICON_OBJ_SPHERE:"Sphere", 
-                ic.ICON_CIRCLE_DOWN:"Pyramid",
+                ic.ICON_OBJ_PYRAMID:"Pyramid",
+                }
+config.callbackL = {
+                ic.ICON_OBJ_BOX:lambda: add_primitive("cube"), 
+                ic.ICON_OBJ_SPHERE:lambda: add_primitive("sphere"), 
+                ic.ICON_OBJ_PYRAMID:lambda: add_primitive("pyramid"),
                 }
 open_context(config)
