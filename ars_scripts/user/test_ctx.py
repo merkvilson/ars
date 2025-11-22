fde118e9ab76a5423168b16bc3a61f3884241980
merkvilson <g.merkvilson@gmail.com>
2025-11-22 19:26:12 +0400

    test scripts

--- ars_scripts/user/test_ctx.py
+++ ars_scripts/user/test_ctx.py
@@ -6,8 +6,9 @@ config.options = {
                 ic.ICON_OBJ_SPHERE:"Sphere", 
                 ic.ICON_OBJ_PYRAMID:"Pyramid",
                 }
+config.slider_values = {ic.ICON_OBJ_BOX:(0,100,50)}
 config.callbackL = {
-                ic.ICON_OBJ_BOX:lambda: add_primitive("cube"), 
+                # ic.ICON_OBJ_BOX:lambda: add_primitive("cube"), 
                 ic.ICON_OBJ_SPHERE:lambda: add_primitive("sphere"), 
                 ic.ICON_OBJ_PYRAMID:lambda: add_primitive("pyramid"),
                 }
