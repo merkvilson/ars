edc0292b61fe140a198a44ea8f85e94e62b2e9b1
merkvilson <g.merkvilson@gmail.com>
2025-11-23 01:06:10 +0400

    feat: implement context menu configuration for primitive object creation

--- ars_scripts/user/test1.py
+++ ars_scripts/user/test1.py
@@ -1,1 +1,17 @@
-1
+from ui.widgets.context_menu import ContextMenuConfig, open_context
+
+config = ContextMenuConfig()
+
+config.options = {
+    ic.ICON_OBJ_BOX:"Cube",
+    ic.ICON_OBJ_SPHERE:"Sphere",
+    ic.ICON_OBJ_PYRAMID:"Pyramid",
+    }
+
+config.callbackL = {
+    ic.ICON_OBJ_BOX:lambda: add_primitive("cube"),
+    ic.ICON_OBJ_SPHERE:lambda: add_primitive("sphere"),
+    ic.ICON_OBJ_PYRAMID:lambda: add_primitive("pyramid"),
+    }
+
+open_context(config)
