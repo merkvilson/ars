d372a20792a071edb1f2495bd41d0474ba0fbe7c
merkvilson <g.merkvilson@gmail.com>
2025-11-23 15:00:30 +0400

    refactor

--- ars_cmds/bubble_cmds/py_scripts.py
+++ ars_cmds/bubble_cmds/py_scripts.py
@@ -1,5 +1,5 @@
 from ui.widgets.context_menu import ContextMenuConfig, open_context
-from ui.widgets.ars_code import CodeEditor
+from ui.ars_code.ars_code import CodeEditor
 from theme.fonts import font_icons as ic
 from ars_cmds.core_cmds.run_ext import run_ext
 from ars_cmds.util_cmds.open_file import open_file
