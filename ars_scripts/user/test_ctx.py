a0833af8cdddfb8ef1f7fb7e7d42b412b40fa33f
merkvilson <g.merkvilson@gmail.com>
2025-11-26 16:13:00 +0400

    fix: update loop variable in get_gui_data function for clarity

--- ars_scripts/user/test_ctx.py
+++ ars_scripts/user/test_ctx.py
@@ -1,4 +1,5 @@
 from ui.widgets.context_menu import open_context, ContextMenuConfig
+from theme.fonts import font_icons as ic
 import os
 import json
 
@@ -10,7 +11,7 @@ airen_class_types = ["Airen_Int"]
 
 test_dict = {}
 def get_gui_data(config):
-    for node_id, node in workflow_template.items():
+    for _, node in workflow_template.items():
         if node.get("class_type") in airen_class_types:
             inputs = node.get("inputs", {})
             if inputs.get("gui_expose"):
