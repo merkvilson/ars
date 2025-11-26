30e0a0bd9197eccac9d86e32f9c57db3e90a3641
merkvilson <g.merkvilson@gmail.com>
2025-11-26 22:34:57 +0400

    refactor: streamline load_json_file and get_gui_data functions for improved readability

--- ars_cmds/bubble_cmds/open_j.py
+++ ars_cmds/bubble_cmds/open_j.py
@@ -7,20 +7,11 @@ from PyQt6.QtWidgets import QFileDialog
 def BBL_J(*args):
     run_ext(__file__)
 
-
-
 def load_json_file(ars_window, file_path=None):
     if file_path is None:
-        file_path, _ = QFileDialog.getOpenFileName(
-            None,
-            "Select Json File",
-            "",
-            "Json Files (*.json)",
-        )
+        file_path, _ = QFileDialog.getOpenFileName(None,"Select Json File","","Json Files (*.json)",)
     if file_path:
         ars_window.prefs.json_ud_path = file_path
-
-
 
 def execute_cmd(ars_window):
 
@@ -32,7 +23,6 @@ def execute_cmd(ars_window):
 
     airen_class_types = ["Airen_Int", "Airen_Float", "Airen_Bool", "Airen_Str"]
 
-    test_dict = {}
     def get_gui_data(config):
         for _, node in workflow_template.items():
             if node.get("class_type") in airen_class_types:
@@ -40,8 +30,11 @@ def execute_cmd(ars_window):
                 if inputs.get("gui_expose"):
                     gui_node_id = inputs["gui_expose"][0]
                     gui_node_inputs = workflow_template.get(gui_node_id).get("inputs")
-                    config.options[ getattr(ic, gui_node_inputs["symbol"]) ] =   gui_node_inputs["additional_text"]
-                    config.slider_values[getattr(ic, gui_node_inputs["symbol"])] =  [float(x) for x in gui_node_inputs["slider_values"].split(",")]
+                    symbol = getattr(ic, gui_node_inputs["symbol"])
+                    additional_text = gui_node_inputs.get("additional_text", "")
+                    slider_values = gui_node_inputs.get("slider_values", "")
+                    config.options[symbol] = additional_text
+                    config.slider_values[symbol] = [float(x) for x in slider_values.split(",")]
 
 
     config=ContextMenuConfig()
