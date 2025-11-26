0b7b524d8f01ba3cdd7ba64d3acd18b1891b01c2
merkvilson <g.merkvilson@gmail.com>
2025-11-27 01:35:41 +0400

    refactor: update airen_class_types and simplify get_gui_data function for improved handling of GUI data

--- ars_cmds/bubble_cmds/open_j.py
+++ ars_cmds/bubble_cmds/open_j.py
@@ -21,20 +21,26 @@ def execute_cmd(ars_window):
 
     with open(ars_window.prefs.json_ud_path, 'r', encoding='utf-8') as f: workflow_template = json.load(f)
 
-    airen_class_types = ["Airen_Int", "Airen_Float", "Airen_Bool", "Airen_Str"]
+    airen_class_types = ["Airen_Gui_Data"]#["Airen_Int", "Airen_Float", "Airen_Bool", "Airen_Str"]
+
+
 
     def get_gui_data(config):
         for _, node in workflow_template.items():
             if node.get("class_type") in airen_class_types:
                 inputs = node.get("inputs", {})
-                if inputs.get("gui_expose"):
-                    gui_node_id = inputs["gui_expose"][0]
-                    gui_node_inputs = workflow_template.get(gui_node_id).get("inputs")
+                if 'slider_values' in inputs:
+                    gui_node_inputs = inputs
+
                     symbol = getattr(ic, gui_node_inputs["symbol"])
                     additional_text = gui_node_inputs.get("additional_text", "")
                     slider_values = gui_node_inputs.get("slider_values", "")
                     config.options[symbol] = additional_text
                     config.slider_values[symbol] = [float(x) for x in slider_values.split(",")]
+
+
+
+
 
 
     config=ContextMenuConfig()
