0e5f262b0b1248207f913caad0a2186d607b0a2e
merkvilson <g.merkvilson@gmail.com>
2025-11-27 01:35:22 +0400

    fix: update test file paths and refine get_gui_data function for Airen_Gui_Data class handling

--- tests/json_read_data/tst.py
+++ tests/json_read_data/tst.py
@@ -1,20 +1,17 @@
 import os
 import json
 
-default_workflow_path = r"C:\Users\gmerk\Downloads\ARS\tests\json_read_data\tst.json"
+default_workflow_path = r"C:\Users\gmerk\Downloads\ARS\tests\json_read_data\test2.json"
 
 with open(default_workflow_path, 'r', encoding='utf-8') as f: workflow_template = json.load(f)
 
-airen_class_types = ["Airen_Int"]
-
-test_dict = {}
 def get_gui_data():
-    for node_id, node in workflow_template.items():
-        if node.get("class_type") in airen_class_types:
+    for _, node in workflow_template.items():
+        if node.get("class_type") in ["Airen_Gui_Data"]:
             inputs = node.get("inputs", {})
-            if inputs.get("gui_expose"):
-                gui_node_id = inputs["gui_expose"][0]
-                gui_node_inputs = workflow_template.get(gui_node_id).get("inputs")
-                print(gui_node_inputs)
+            if 'ud_name' in inputs:
+                print(inputs)
 
 get_gui_data()
+
+# print(workflow_template)
