0ad881f839a212176ae051734cd855b83deec964
merkvilson <g.merkvilson@gmail.com>
2025-12-01 18:57:39 +0400

    feat: add newline at end of file and ensure consistent formatting in delete_files.py

--- ars_cmds/util_cmds/delete_files.py
+++ ars_cmds/util_cmds/delete_files.py
@@ -5,3 +5,8 @@ def delete_all_files_in_folder(folder_path):
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
         if os.path.isfile(file_path): os.remove(file_path)
+
+
+def delete_single_file(file_path):
+    if os.path.exists(file_path) and os.path.isfile(file_path):
+        os.remove(file_path)
