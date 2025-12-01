7d9d766420997a5243aaa4a3e457f9b9457b036c
merkvilson <g.merkvilson@gmail.com>
2025-12-01 20:22:41 +0400

    feat: update set_text_from_prompt to store prompt in ars_window

--- ars_cmds/bubble_cmds/prompt_editor_cmd.py
+++ ars_cmds/bubble_cmds/prompt_editor_cmd.py
@@ -38,6 +38,7 @@ def execute_cmd(ars_window):
     def set_text_from_prompt():
         ars_window.prefs.json_positive = editor.toPlainText()
         ars_window.prefs.render_prompt = editor.toPlainText()
+        ars_window.prompt = editor.toPlainText()
 
 
     editor = PromptEditor()
