651d2da9eea4e985ac2b76e8862fd1305d2fc5f9
merkvilson <g.merkvilson@gmail.com>
2025-12-02 18:19:23 +0400

    feat: implement context menu execution with options in new.py

--- ars_scripts/user/new.py
+++ ars_scripts/user/new.py
@@ -1,1 +1,14 @@
-print("test")
+from ui.widgets.context_menu import ContextMenuConfig, open_context
+# from ars_cmds.core_cmds.run_ext import run_ext
+# from theme.fonts import font_icons as ic
+
+
+def execute(ars_window):
+    config = ContextMenuConfig()
+    config.options = {
+        ic.ICON_TEST: "Option A",
+        ic.ICON_TEST2: "Option B",
+        ic.ICON_TEST3: "Option C",}
+    ctx = open_context(config)
+
+execute(ars_window)
