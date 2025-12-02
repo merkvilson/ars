ba1e9e3cc7c9af621f95bf7d53a31eab2a1e1015
merkvilson <g.merkvilson@gmail.com>
2025-12-02 18:51:05 +0400

    refactor: move get_local_ip and start_comfy functions to top level in start_comfy_opt.py

--- ars_cmds/bubble_cmds/start_comfy_opt.py
+++ ars_cmds/bubble_cmds/start_comfy_opt.py
@@ -9,6 +9,21 @@ import pyperclip
 def BBL_C(*args):
     run_ext(__file__)
 
+def get_local_ip():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    return ip
+
+
+def start_comfy(server = False, cpu = False):
+    cui_root = read_pref("cui_root")
+    cui_python = os.path.join(cui_root, "python_embeded", "python.exe")
+    cmd = f'"{cui_python}" -s "{os.path.join(cui_root, "ComfyUI", "main.py")}" {"--cpu" if cpu else ""} --windows-standalone-build {"--listen 0.0.0.0 --port 8188" if server else ""}'
+    subprocess.Popen(cmd, shell=True)  # Use Popen instead of run to avoid blocking
+    if server:
+        print(f"ComfyUI server started at http://{get_local_ip()}:8188")
+        pyperclip.copy(f"http://{get_local_ip()}:8188")
+        
     
 
 def execute_cmd(ars_window):
@@ -22,26 +37,8 @@ def execute_cmd(ars_window):
         "c": "CPU"
     }
 
-    def get_local_ip():
-        hostname = socket.gethostname()
-        ip = socket.gethostbyname(hostname)
-        return ip
 
-
-    def start_comfy(server = False, cpu = False):
-        cui_root = read_pref("cui_root")
-        cui_python = os.path.join(cui_root, "python_embeded", "python.exe")
-        cmd = f'"{cui_python}" -s "{os.path.join(cui_root, "ComfyUI", "main.py")}" {"--cpu" if cpu else ""} --windows-standalone-build {"--listen 0.0.0.0 --port 8188" if server else ""}'
-        subprocess.Popen(cmd, shell=True)  # Use Popen instead of run to avoid blocking
-        if server:
-            print(f"ComfyUI server started at http://{get_local_ip()}:8188")
-            pyperclip.copy(f"http://{get_local_ip()}:8188")
-            
-
-    config.callbackL = {
-        "a": lambda: (start_comfy(server=ctx.get_value("b"), cpu=ctx.get_value("c")),
-                      ctx.close_animated()),
-                        }
+    config.callbackL = { "a": lambda: start_comfy(server=ctx.get_value("b"), cpu=ctx.get_value("c"))}
 
     ctx = open_context(
         items=options_list,
