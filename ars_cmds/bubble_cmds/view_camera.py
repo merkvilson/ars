367eda43000021afbfef29995abf3e859d251a8d
merkvilson <g.merkvilson@gmail.com>
2025-12-09 22:56:09 +0400

    feat: Enhance camera viewer with cursor positioning and context menu options

--- ars_cmds/bubble_cmds/view_camera.py
+++ ars_cmds/bubble_cmds/view_camera.py
@@ -2,6 +2,7 @@ from ui.widgets.context_menu import ContextMenuConfig, open_context
 from ars_cmds.core_cmds.run_ext import run_ext
 from theme.fonts import font_icons as ic
 from ars_cmds.core_cmds.load_object import selected_object as get_selected
+from ars_cmds.core_cmds.cursor_to_xyz import get_xyz
 
 BBL_CAMVIEWER_CONFIG = {"symbol": ic.ICON_WINDOW_MINIMIZE, "hotkey": "V"}
 
@@ -13,12 +14,22 @@ def BBL_CAMVIEWER(*args):
 def execute_cmd(ars_window):
     config = ContextMenuConfig()
     config.options = {
-        ic.ICON_HOME: "View Home",
-        ic.ICON_WINDOW_FULLSCREEN: "View Selected",
+        ic.ICON_HOME: "Home",
+        ic.ICON_WINDOW_FULLSCREEN: "Selected",
+        ic.ICON_CIRCLE_DASHED: "Cursor"
     }
 
     camera = ars_window.viewport.cam
     default_rotation = (camera._reset_rotation1, camera._reset_rotation2)
+
+    def view_cursor():
+        new_xyz = get_xyz(ars_window)
+        #TODO: add command to get object under cursor
+        if get_selected():
+            scale_sum = sum(get_selected().get_scale()) * 2
+        else:
+            scale_sum = 10
+        camera.move_to(center=new_xyz, offset=10, animate=True)
 
 
     def view_selected():
@@ -34,11 +45,13 @@ def execute_cmd(ars_window):
     config.callbackL = {
         ic.ICON_HOME: view_home,
         ic.ICON_WINDOW_FULLSCREEN: view_selected,
+        ic.ICON_CIRCLE_DASHED: view_cursor,
     }
 
     config.hotkey_items = {
         ic.ICON_HOME: "H",
         ic.ICON_WINDOW_FULLSCREEN: "V",
+        ic.ICON_CIRCLE_DASHED: "C",
         
     }
 
