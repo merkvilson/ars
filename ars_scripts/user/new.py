70dfe09e1797e9f796aded223992806a8a513dce
merkvilson <g.merkvilson@gmail.com>
2025-12-10 02:17:57 +0400

    feat: Replace object movement with cube addition using cursor position

--- ars_scripts/user/new.py
+++ ars_scripts/user/new.py
@@ -1,3 +1,3 @@
-my_object = get_selected()
-my_object.move_to(center=(10, 0, 0), offset=2.0, animate=True)
-
+from ars_cmds.core_cmds.cursor_to_xyz import get_xyz
+p = get_xyz(ars_window)
+add_primitive('cube', position=(p[0],p[1]+1,p[2]),animated=0 )
