a8e3063933b032683ccc85900860d75fe2ede5ca
merkvilson <g.merkvilson@gmail.com>
2025-12-10 20:20:12 +0400

    feat: Change cursor style to map-pin during camera view activation

--- ars_cmds/bubble_cmds/view_camera.py
+++ ars_cmds/bubble_cmds/view_camera.py
@@ -73,7 +73,7 @@ def execute_cmd(ars_window):
     def start():
         nonlocal start_time
         start_time = time.time()
-        set_cursor("point", "center")
+        set_cursor("map-pin", "center")
         ars_window.hotkey_manager._unbind_all()
 
     
