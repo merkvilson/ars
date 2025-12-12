862513fed24d0bb5baf90a13480f9c1af9311c0f
merkvilson <g.merkvilson@gmail.com>
2025-12-12 21:45:12 +0400

    fix: Simplify main function in startup tests by removing unused imports and comments

--- tests/startup_test.py
+++ tests/startup_test.py
@@ -1,9 +1,13 @@
-from ars_cmds.core_cmds.load_object import add_primitive, selected_object
-from ars_cmds.util_cmds.time_cmd import after, delay
+from ars_cmds.core_cmds.load_object import add_primitive
+from ars_cmds.util_cmds.time_cmd import after
+from ars_cmds.render_cmds.render_pass import save_normal
+
+
 def main(ars_window):
-    print("Running startup tests...")
-
-
+    
+    add_primitive("cube")
+    # save_normal(ars_window)
+    # print("finish")
 
 def run_tests(ars_window):
     after(3000, lambda: main(ars_window))
