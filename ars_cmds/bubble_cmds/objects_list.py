c6be2e9fcb61434c75f3851bab8987ad6a377287
merkvilson <g.merkvilson@gmail.com>
2025-12-22 18:44:43 +0400

    refactor: Update execute_cmd to enhance context menu options and improve code organization

--- ars_cmds/bubble_cmds/objects_list.py
+++ ars_cmds/bubble_cmds/objects_list.py
@@ -3,6 +3,8 @@ from theme.fonts import font_icons as ic
 from ars_cmds.core_cmds.run_ext import run_ext
 from ui.widgets.hierarchy_tree import ObjectHierarchyWindow
 
+from ars_cmds.bubble_cmds.delete_selected_obj import BBL_TRASH as delete_cmd
+from ars_cmds.bubble_cmds.view_camera import view_selected
 
 BBL_LIST_CONFIG ={"symbol": ic.ICON_LIST }
 def BBL_LIST(*args):
@@ -15,22 +17,26 @@ def execute_cmd(ars_window):
     config.auto_close = False
     config.close_on_outside = False
     config.extra_distance=[9999,0]
+    config.use_extended_shape = False
+    config.distribution_mode = "x"
 
 
-    options_list = ["1", "hierarchy", "   "]
-    
+    options_list = [
+        [ic.ICON_SETTINGS, ic.ICON_TRASH, ic.ICON_FOCUS, ic.ICON_OBJ_BBOX], 
+        ["hierarchy"],
+        "   ",
+        ]
     
     hierarchy = ObjectHierarchyWindow(ars_window.viewport)
 
     config.custom_widget_items = {"hierarchy": hierarchy}
 
-    config.additional_texts = {
-    "1": "Close Hierarchy",
-    }
-
 
     config.callbackL = {
         "1": lambda: ctx.close_animated(),
+
+        ic.ICON_TRASH: lambda: delete_cmd(ars_window),
+        ic.ICON_FOCUS: lambda: view_selected(),
     }
 
     ctx = config.open_context(items=options_list)
