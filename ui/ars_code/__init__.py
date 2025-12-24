81a5605adda25e078b21905dc129a0260384371a
merkvilson <g.merkvilson@gmail.com>
2025-12-24 05:14:01 +0400

    fix: Import CodeEditorWidget in __init__.py for proper module access

--- ui/ars_code/__init__.py
+++ ui/ars_code/__init__.py
@@ -3,4 +3,5 @@ from .completer import JediCompleter, CompletionDelegate, CompletionPopup
 from .editor import CodeEditor, LineNumberArea
 from .prompt_editor import PromptEditor
 from .terminal import TerminalWidget
+from .code_editor_widget import CodeEditorWidget
 
