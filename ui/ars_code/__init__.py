36a636cbc82d278e1407e34a12cca60fb67b1d89
merkvilson <g.merkvilson@gmail.com>
2025-12-24 05:15:24 +0400

    refactor: Rename PromptEditor to PromptEditorWidget

--- ui/ars_code/__init__.py
+++ ui/ars_code/__init__.py
@@ -1,7 +1,7 @@
 from .highlighter import PythonHighlighter
 from .completer import JediCompleter, CompletionDelegate, CompletionPopup
 from .editor import CodeEditor, LineNumberArea
-from .prompt_editor import PromptEditor
+from .prompt_editor_widget import PromptEditor
 from .terminal import TerminalWidget
 from .code_editor_widget import CodeEditorWidget
 
