1fa2cce950da9eff0d05b1a9292a32a06f679524
merkvilson <g.merkvilson@gmail.com>
2025-12-25 08:08:15 +0400

    feat: Update BBL_top_row function and context menu items for improved configuration

--- ars_cmds/bubble_cmds/top_row.py
+++ ars_cmds/bubble_cmds/top_row.py
@@ -3,8 +3,8 @@ from theme.fonts import font_icons as ic
 from ars_cmds.core_cmds.run_ext import run_ext
 
 
-# BBL_top_row_CONFIG ={"symbol": ic.ICON_, "visible": False }
-def BBL_L(*args):
+BBL_top_row_CONFIG ={"symbol": "", }
+def BBL_top_row(*args):
     run_ext(__file__)
 
 
@@ -13,12 +13,14 @@ def execute_cmd(ars_window):
     config.dock_area = "top"
     config.auto_close = False
     config.close_on_outside = False
-    config.background_color = (0, 0, 0, 5)
     config.use_extended_shape = False
-    config.item_radius = 5 
+    config.distribution_mode = "x"
 
 
-    config.options = {"": ""}
+    items = [ ic.ICON_SETTINGS, ic.ICON_LIST, "   ", ic.ICON_CAMERA, ic.ICON_EYE, ic.ICON_RENDER, ic.ICON_BRAIN, ic.ICON_OBJ_BBOX, ic.ICON_CODE_TERMINAL, ic.ICON_WORKFLOW, ic.ICON_BACKGROUND,ic.ICON_PLAYER_TRACK_NEXT, "   ",  ic.ICON_BUBBLE,ic.ICON_MENU,]
+
+
     
-    ctx = config.open_context(parent=ars_window.central_widget)
+    
+    ctx = config.open_context(parent=ars_window.central_widget, items=items)
 
