d183d1dee1b7a5c60fe10945f8559a97a4d711be
merkvilson <g.merkvilson@gmail.com>
2025-12-25 06:41:35 +0400

    feat: Refactor execute_cmd by removing unused imports and simplifying context menu item naming

--- ars_cmds/bubble_cmds/objects_list.py
+++ ars_cmds/bubble_cmds/objects_list.py
@@ -2,8 +2,6 @@ from ui.widgets.context_menu import CtxConfig
 from theme.fonts import font_icons as ic
 from ars_cmds.core_cmds.run_ext import run_ext
 from ui.widgets.hierarchy_tree import ObjectHierarchyWindow
-from PyQt6.QtWidgets import QSizePolicy
-from PyQt6.QtCore import Qt
 
 
 BBL_LIST_CONFIG ={"symbol": ic.ICON_LIST }
@@ -16,14 +14,12 @@ def execute_cmd(ars_window):
     config.dock_area = "left"
     config.auto_close = False
     config.close_on_outside = False
-    
-    # Set width from prefs or default
-    config.custom_width = getattr(ars_window.prefs, 'objects_list_width', 300)
-    
+
+    # Create Object Hierarchy Widget    
     hierarchy = ObjectHierarchyWindow(ars_window.viewport)
 
-    config.custom_widget_items = {"Hierarchy": hierarchy}
-    options_list =["Hierarchy"]
+    config.custom_widget_items = {"hierarchy": hierarchy}
+    options_list =["hierarchy"]
     
     ctx = config.open_context(items=options_list, parent=ars_window.central_widget)
 
