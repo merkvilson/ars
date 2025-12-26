17e3f0244cde6ba92638141e0c52e9f0aede3cd8
merkvilson <g.merkvilson@gmail.com>
2025-12-26 11:36:29 +0400

    feat: Add initial implementation of bubble command functionality

---
+++ ars_cmds/bubble_cmds/test_bbl.py
@@ -0,0 +1,21 @@
+from ui.widgets.context_menu import CtxConfig
+from theme.fonts import font_icons as ic
+from ars_cmds.core_cmds.run_ext import run_ext
+from PyQt6.QtGui import QCursor
+
+BBL_T_CONFIG = {"symbol": "T",}
+
+def BBL_T(*arg):
+    run_ext(__file__)
+
+
+def execute_cmd(ars_window):
+    config = CtxConfig(options = {
+        ic.ICON_AXIS_X: "X",
+        ic.ICON_AXIS_Y: "Y",
+        ic.ICON_AXIS_Z: "Z",
+        ic.ICON_GRID: "grid",
+        ic.ICON_GIZMO_AXIS_3D: "xyz",
+    })
+    
+    ctx = config.open_context()
