d969a1f326d0607e58ad625d997054187a420a59
merkvilson <g.merkvilson@gmail.com>
2025-12-28 01:55:11 +0400

    refactor: Clean up startup_test.py by removing commented code and retaining essential print statement

--- ars_cmds/startup_cmds/startup_test.py
+++ ars_cmds/startup_cmds/startup_test.py
@@ -1,10 +1,5 @@
-from ars_cmds.core_cmds.load_object import add_primitive
-from ars_cmds.util_cmds.time_cmd import after
-# from ars_cmds.render_cmds.render_pass import save_normal
+"""
+This module will be executed during startup to run tests.
+"""
 
-
-# add_primitive("cube").set_position(0, 0, -3)
-# add_primitive("sphere").set_position(2, 0, -3)
-# add_primitive("cone").set_position(-2, 0, -3)
-
-# print("finish")
+print("Startup command executed")
