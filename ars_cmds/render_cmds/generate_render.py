6347c6e5864352603f676f20e76fb8b67069f191
merkvilson <g.merkvilson@gmail.com>
2025-12-30 06:10:15 +0400

    feat: Update PromptEditorWidget with the latest image in generate_render method

--- ars_cmds/render_cmds/generate_render.py
+++ ars_cmds/render_cmds/generate_render.py
@@ -95,6 +95,7 @@ def generate_render(self, ctx, max_steps, default_object):
                             self.viewport.bg.set_image(file_to_apply)
                         else:
                             ctx.update_item(ic.ICON_IMAGE, "image_path", file_to_apply)
+                            ctx.update_custom_widget("PromptEditorWidget", lambda widget: widget.set_image(file_to_apply))
                         if not self.viewport.isVisible() and hasattr(self, 'img') and self.img:
                             self.img.open_image(file_to_apply)
                 except:
