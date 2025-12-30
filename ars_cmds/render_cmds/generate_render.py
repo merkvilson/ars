f4d4806d3cf084a684119ec04b609fd4b58807db
merkvilson <g.merkvilson@gmail.com>
2025-12-30 13:52:28 +0400

    feat: Decrease timer interval for texture application from 300ms to 150ms

--- ars_cmds/render_cmds/generate_render.py
+++ ars_cmds/render_cmds/generate_render.py
@@ -109,6 +109,6 @@ def generate_render(self, ctx, max_steps, default_object):
         
     # Start the timer
     update_timer.timeout.connect(apply_latest_texture)
-    update_timer.start(300)
+    update_timer.start(150)
     
     self._render_timer = update_timer
